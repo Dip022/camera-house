230d3e52159209002acd177c7348a3f1180d9031
Dipok Roy <roydipok022@gmail.com>
2022-03-26 00:34:23 +0600

    header component added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Camera House</h1>
+      <h3>You can take the camera of your choice</h3>
+    </div>
+  );
+};
+
+export default Header;
